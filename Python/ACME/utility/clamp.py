e6f31f9fdb57315b7c00c4d89ec59dedf470c486
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-05-28 17:43:29 +0200

    Improved code

--- Python/ACME/utility/clamp.py
+++ Python/ACME/utility/clamp.py
@@ -1,9 +1,10 @@
+import math
 from .size import *
 from .ndim import *
 
 def clamp(a,inf=0,sup=1):
     """
-    Returns the clamped value of the input in range [inf-sup]
+    Returns the clamped value of the input in range [inf,sup]
 
     Parameters
     ----------
@@ -23,3 +24,46 @@ def clamp(a,inf=0,sup=1):
     if istorch(a):
         return torch.clamp(a,inf,sup)
     return min(max(inf,a),sup)
+
+
+
+def clampmax(a,sup=1):
+    """
+    Returns the clamped value of the input in range [-\infty,sup]
+
+    Parameters
+    ----------
+    a : int,float or Tensor
+        input value
+    sup : int or float (optional)
+        the superior bound (default is 1)
+
+    Returns
+    -------
+    int or float or Tensor
+        the clamped input
+    """
+
+    return clamp(a,inf=-math.inf,sup=sup)
+
+
+
+def clampmin(a,inf=0):
+    """
+    Returns the clamped value of the input in range [inf,\infty]
+
+    Parameters
+    ----------
+    a : int,float or Tensor
+        input value
+    inf : int or float (optional)
+        the inferior bound (default is 0)
+
+    Returns
+    -------
+    int or float or Tensor
+        the clamped input
+    """
+
+    return clamp(a,inf=inf,sup=math.inf)
+
