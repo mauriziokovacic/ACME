7fd67439a498146d4f963d635e3b2fc8da4dc0fe
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-05-28 10:23:06 +0200

    Added new definition

---
+++ Python/ACME/utility/Uint8Tensor.py
@@ -0,0 +1,20 @@
+import torch
+
+def Uint8Tensor(values,device='cuda:0'):
+    """
+    Returns a Tensor of type torch.uint8 containing the given values
+
+    Parameters
+    ----------
+    values : list
+        the values of the tensor
+    device : str
+        the device to store the tensor to
+
+    Returns
+    -------
+    Tensor
+        a unsigned integer 8 precision tensor
+    """
+
+    return torch.tensor(values,dtype=torch.uint8,device=device)
