5bd0f3e017807c21e24510ad5ad8f0bf66cc4216
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-05-31 11:08:26 +0200

    Fixed doc and improved code

--- Python/ACME/utility/circshift.py
+++ Python/ACME/utility/circshift.py
@@ -1,4 +1,7 @@
+import numpy
 import torch
+from .isnumpy import *
+from .istorch import *
 
 def circshift(tensor,k,dim=None):
     """
@@ -12,6 +15,20 @@ def circshift(tensor,k,dim=None):
         the number of shifts to perform
     dim : int (optional)
         the dimension along the shift is performed (default is None)
+
+    Returns
+    -------
+    Tensor
+        the tensor with shifted values
+
+    Raises
+    ------
+    AssertionError
+        if input tensor is neither a Numpy or PyTorch tensor
     """
 
-    return torch.roll(tensor,k,dims=dim)
+    if isnumpy(tensor):
+        return numpy.roll(tensor,k,axis=dim)
+    if istorch(tensor):
+        return torch.roll(tensor,k,dims=dim)
+    assert False, 'Unknown data type'
