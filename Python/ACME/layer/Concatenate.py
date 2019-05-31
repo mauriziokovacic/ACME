9b91d261e5e573ece95eeb4f32aca2ebfdcf06dd
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-05-31 14:53:30 +0200

    Fixed AggregationLayer

--- Python/ACME/layer/Concatenate.py
+++ Python/ACME/layer/Concatenate.py
@@ -20,7 +20,7 @@ class Concatenate(torch.nn.Module):
         Parameters
         ----------
         dim : int (optional)
-            the dimension along the concatenation is performed
+            the dimension along the concatenation is performed (default is 0)
         """
 
         super(Concatenate,self).__init__()
@@ -44,3 +44,56 @@ class Concatenate(torch.nn.Module):
         """
 
         return torch.cat(*inputs,dim=self.dim)
+
+
+
+class AggregationLayer(Concatenate):
+    """
+    Aggregates the outputs of a sequence of models
+
+    Attributes
+    ----------
+    models : torch.nn.Module...
+        a sequence of modules
+
+    Methods
+    -------
+    forward(*inputs)
+        forwards the input(s) to each contained model
+    """
+
+    def __init__(self,*models,dim=0):
+        """
+        Parameters
+        ----------
+        *models : torch.nn.Module...
+            a sequence of modules
+        dim : int (optional)
+            the dimension along the concatenation is performed (default is 0)
+        """
+
+        super(AggregationLayer,self).__init__()
+        self.models = torch.nn.ModuleList(*models)
+
+
+
+    def forward(self,*inputs):
+        """
+        Forwards the input(s) to each contained model
+
+        Parameters
+        ----------
+        *inputs : object...
+            a sequence of objects
+
+        Returns
+        -------
+        Tensor
+            the concatenated output of each model
+        """
+
+        if len(inputs)==1:
+            out = [f(*inputs) for f in self.models]
+        else:
+            out = [f(i) for f,i in zip(self.models,inputs)]
+        return super(AggregationLayer,self).forward(*out)
