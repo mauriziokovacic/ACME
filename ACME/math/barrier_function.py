95439d4b7b8d694b8c464a64cb989a6cd4e01938
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-06 09:24:50 +0200

    Fixed definition

--- ACME/math/barrier_function.py
+++ ACME/math/barrier_function.py
@@ -1,18 +1,27 @@
+from .constant import *
+
 def barrier_function(x,t):
     """
     Returns the barrier function for a given value x and a threshold t
 
     Parameters
     ----------
-    x : scalar or Tensor
+    x : Tensor
         the input value
     t : scalar
         the threshold value from which the barrier starts
 
     Returns
     -------
-    scalar or Tensor
+    Tensor
         the barrier vlaue
     """
 
-    return (x**3)/(t**3) - 3*(x**2)/(t**2) + 3*x/t
+    def g(x):
+        return (x**3)/(t**3) - 3*(x**2)/(t**2) + 3*x/t
+
+    out       = torch.zeros(row(x),dtype=torch.float,device=x.device)
+    i         = (x>0)and(x<t)
+    out[i]    = torch.reciprocal(g(x[i])) - 1
+    out[x<=0] = inf
+    return out
