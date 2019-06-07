ad0ff9d64e577256cf613657ca8925d08d5a4328
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-07 09:08:29 +0200

    Added new layer

---
+++ ACME/layer/MoveLayer.py
@@ -0,0 +1,46 @@
+from torch.nn import Module
+
+class MoveLayer(torch.nn.Module):
+    """
+    A layer moving the input to a specifc device
+
+    Attributes
+    ----------
+    device : str or torch.device
+        the device to move the input to
+
+    Methods
+    -------
+    forward(input)
+        returns the moved input
+    """
+
+    def __init__(self,device):
+        super(MoveLayer, self).__init__()
+        """
+        Parameters
+        ----------
+        device : str or torch.device
+            the device to move the input to
+        """
+
+        self.device = device
+
+
+
+    def forward(self, input):
+        """
+        Returns the moved input
+
+        Parameters
+        ----------
+        input : object
+            the input object
+
+        Returns
+        -------
+        object
+            the moved input
+        """
+
+        return input.to(device=self.device)
