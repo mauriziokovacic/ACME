765b273862f0b8992c558589f9a051133b77287c
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-11 16:35:28 +0200

    Fixed code

--- ACME/geometry/sphere_coverage.py
+++ ACME/geometry/sphere_coverage.py
@@ -10,7 +10,7 @@ def sphere_coverage(nsamples,dtype=torch.float,device='cuda:0'):
     phi  = i/SQRT3
     phi  = (phi-torch.floor(phi))*PI2
     cosT = (i*2+1)/nsamples-1
-    sinT = torch.sqrt(torch.clamp(1-torch.pow(c,2),min=0,max=1))
+    sinT = torch.sqrt(torch.clamp(1-torch.pow(cosT,2),min=0,max=1))
     return torch.cat((cos(phi)*sinT,sin(phi)*sinT,cosT),dim=1)
 
 
