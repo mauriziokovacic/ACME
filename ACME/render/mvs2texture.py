ef63c41704f0a04e360cbe3ac1f96eeaac23e414
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-12 09:35:01 +0200

    Added functionality

---
+++ ACME/render/mvs2texture.py
@@ -0,0 +1,16 @@
+def mvs2texture(I):
+    """
+    Converts a Multi View Stack tensor into a 3D texture
+
+    Parameters
+    ----------
+    I : Tensor
+        a Multi View Stack tensor
+
+    Returns
+    -------
+    Tensor
+        a 3D texture tensor
+    """
+
+    return I.permute(1,0,2,3)
