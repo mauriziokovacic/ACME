362a35b880465c4272306d5e4ab0d768f2a859fc
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-13 16:48:22 +0200

    Fixed Renderer definition and nr2img

--- ACME/render/nr2img.py
+++ ACME/render/nr2img.py
@@ -1,18 +1,16 @@
-from ACME.utility.torch2numpy import *
-
 def nr2img(I):
     """
-    Converts the output of the Neural Renderer into a Numpy RGB image
+    Converts the output of the Neural Renderer into an RGB image
 
     Parameters
     ----------
     I : Tensor
-        the output PyTorch tensor of Neural Renderer
+        the (1,C,W,H,) tensor
 
     Returns
     -------
     Tensor
-        a Numpy tensor
+        a (W,H,C,) tensor
     """
 
-    return torch2numpy(I.permute(3,2,1,0))[:,:,0:3,0]
+    return I.permute(3,2,1,0)[:,:,0:3,0]
--- ACME/render/renderer.py
+++ ACME/render/renderer.py
@@ -1,76 +1,74 @@
 import neural_renderer as nr
 from ACME.math.unitvec import *
 
-def toggle_lighting(renderer,status):
-    """
-    Toggles the directional lighting of the input renderer on or off depending on status
 
-    Parameters
+
+class Renderer(nr.Renderer):
+    """
+    A class extending the Neural Renderer
+
+    Attributes
     ----------
-    renderer : Neural Renderer
-        an instance of a Neural Renderer
-    status : bool
-        the lighting status
-
-    Returns
-    -------
-    None
-    """
-
-    if status:
-        renderer.light_intensity_directional = 0.5
-    else:
-        renderer.light_intensity_directional = 0
-    renderer.light_intensity_ambient = 1-renderer.light_intensity_directional
-
-
-
-def enable_lighting(renderer):
-    """
-    Enables the lighting of the input renderer
-
-    Parameters
-    ----------
-    renderer : Neural Renderer
-        an instance of a Neural Renderer
-    """
-
-    toggle_lighting(renderer,True)
-
-
-
-def disable_lighting(renderer):
-    """
-    Disables the lighting of the input renderer
-
-    Parameters
-    ----------
-    renderer : Neural Renderer
-        an instance of a Neural Renderer
-    """
-
-    toggle_lighting(renderer,False)
-
-
-
-def create_renderer(camera_mode='look_at',image_size=256,lighting=False,device='cuda:0'):
-    """
-    Creates an instance of a Neural Renderer
-
-    Parameters
-    ----------
-    camera_mode : str (optional)
-        the camera mode of the renderer (default is 'look_at')
-    image_size : int (optional)
-        the output image size resolution (default is 256)
-    lighting : bool (optional)
-        if True turns of the directional lighting of the renderer (default is False)
     device : str or torch.device (optional)
         the tensor the renderer will be stored to (default is 'cuda:0')
+
+    Methods
+    -------
+    toggle_lighting(status)
+        toggles the renderer directional lighting on or off depending on status
+    enable_lighting()
+        enables the renderer lighting
+    disable_lighting()
+        disables the renderer lighting
     """
 
-    R                 = nr.Renderer(camera_mode='look_at',image_size=image_size, device=device)
-    R.eye             = unitvec(3,0,device=device)
-    R.light_direction = -R.eye
-    toggle_lighting(R,lighting)
-    return R
+    def __init__(self,device='cuda:0',lighting=False,**kwargs):
+        """
+        Parameters
+        ----------
+        device : str or torch.device (optional)
+            the device the tensors will be stored to (default is 'cuda:0')
+        lighting : bool (optional)
+            if True activates the lighting, False otherwise
+        **kwargs : ...
+            the Neural Renderer keyworded arguments
+        """
+
+        super(Renderer,self).__init__(camera_mode='look_at',**kwargs)
+        self.eye             = unitvec(3,0,device=device)
+        self.light_direction = -self.eye
+        self.device          = device
+        self.toggle_lighting(lighting)
+
+
+
+    def toggle_lighting(self,status):
+        """
+        Toggles the renderer directional lighting on or off depending on status
+
+        Parameters
+        ----------
+        status : bool
+            the lighting status
+        """
+
+        if status:
+            self.light_intensity_directional = 0.5
+        else:
+            self.light_intensity_directional = 0
+        self.light_intensity_ambient = 1-self.light_intensity_directional
+
+
+
+    def enable_lighting(self):
+        """Enables the renderer lighting"""
+
+        self.toggle_lighting(True)
+
+
+
+    def disable_lighting(self):
+        """Disables the renderer lighting"""
+
+        self.toggle_lighting(False)
+
