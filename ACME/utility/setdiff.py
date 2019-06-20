19bc5724b30d749669272b80b4d305a0f7a6b7cd
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-20 10:06:09 +0200

    Fixed typo and added doc

--- ACME/utility/setdiff.py
+++ ACME/utility/setdiff.py
@@ -1,5 +1,21 @@
 import torch
 from .flatten import *
 
-def setdiff(A,B)
+def setdiff(A,B):
+    """
+    Returns the set difference tensor with all the elements in A not belonging to B
+
+    Parameters
+    ----------
+    A : Tensor
+        the first input tensor
+    B : Tensor
+        the second input tensor
+
+    Returns
+    -------
+    Tensor
+        the set difference tensor
+    """
+
     return torch.tensor(list(set(flatten(A))-set(flatten(B))),dtype=A.dtype,device=A.device)
