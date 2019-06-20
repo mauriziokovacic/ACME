158bbdfe2eee4a535c1316c66b15f81946bab208
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-20 10:16:36 +0200

    Changed wrong name

--- ACME/color/colors.py
+++ ACME/color/colors.py
@@ -1,6 +1,6 @@
 import torch
 from ACME.utility.Uint8Tensor import *
-from .color2double       import *
+from .color2float       import *
 
 def black():
     return torch.zeros(1,3)
@@ -9,13 +9,13 @@ def white():
     return torch.ones(1,3)
 
 def red():
-    return color2double(Uint8Tensor([237,28,36]))
+    return color2float(Uint8Tensor([237,28,36]))
 
 def green():
-    return color2double(Uint8Tensor([34,177,76]))
+    return color2float(Uint8Tensor([34,177,76]))
 
 def blue():
-    return color2double(Uint8Tensor([0,162,232]))
+    return color2float(Uint8Tensor([0,162,232]))
 
 def cyan():
     return torch.add(torch.neg(red()),1)
@@ -27,19 +27,19 @@ def yellow():
     return torch.add(torch.neg(blue()),1)
 
 def brown():
-    return color2double(Uint8Tensor([149,116,83]))
+    return color2float(Uint8Tensor([149,116,83]))
 
 def darkteal():
-    return color2double(Uint8Tensor([98,140,178]))
+    return color2float(Uint8Tensor([98,140,178]))
 
 def grey():
     return torch.mul(torch.ones(1,3),0.5)
 
 def orange():
-    return color2double(Uint8Tensor([253,135,86]))
+    return color2float(Uint8Tensor([253,135,86]))
 
 def pink():
-    return color2double(Uint8Tensor([254,194,194]))
+    return color2float(Uint8Tensor([254,194,194]))
 
 def teal():
-    return color2double(Uint8Tensor([144,216,196]))
+    return color2float(Uint8Tensor([144,216,196]))
