f0d445244988d31c6703868caf5e62cf16e36320
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-06-20 10:05:50 +0200

    Improved code

--- ACME/utility/matmul.py
+++ ACME/utility/matmul.py
@@ -1,11 +1,14 @@
 import numpy
 import torch
-from .isnumpy import *
-from .istorch import *
+from .isnumpy  import *
+from .istorch  import *
+from .issparse import *
 
 def matmul(tensor_a,tensor_b):
     if isnumpy(tensor_a,tensor_b):
         return numpy.matmul(tensor_a,tensor_b)
     if istorch(tensor_a,tensor_b):
+        if issparse(tensor_a):
+            return torch.sparse.mm(tensor_a,tensor_b)
         return torch.mm(tensor_a,tensor_b)
     assert False, 'Unknown data type'
