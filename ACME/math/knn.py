85b0ae426b2c4e29d196db14d8bafa3ba0b9f99a
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-07-01 09:35:14 +0200

    Solved import problem and improved code

--- ACME/math/knn.py
+++ ACME/math/knn.py
@@ -1,8 +1,7 @@
 import torch
-from .norm              import *
 from .prepare_broadcast import *
 
-def knn(A, B, k, distFcn=distance):
+def knn(A, B, k, distFcn):
     """
     Returns the indices of the k-nearest neighbors of A in B
 
@@ -14,10 +13,24 @@ def knn(A, B, k, distFcn=distance):
         a (M,F,) tensor
     k : int
         the number of neighbors to find
-    distFcn : callable (optional)
-        the distance function to use (default is euclidean norm)
+    distFcn : callable
+        the distance function to use
+
+    Returns
+    -------
+    (LongTensor, Tensor)
+        the indices of the k neaighbors and their distance
+
+    Raises
+    ------
+    AssertionError
+        if k is lower than 1
     """
 
+    assert k>0, 'k value must be greater than 0'
     a, b = prepare_broadcast(A, B)
-    d, i = torch.topk(-distFcn(a, b, dim=-1), k, dim=1)
-    return i, -d
+    if k>1:
+        d, i = torch.topk(-distFcn(a, b, dim=-1), k, dim=1)
+        return i, -d
+    d, i = torch.min(distFcn(a, b, dim=-1))
+    return i, d
