3cfc69508b01f52110de40eb48e880e7cbe9d656
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-07-01 10:26:22 +0200

    Change code to have gradient

--- ACME/geometry/soup2mesh.py
+++ ACME/geometry/soup2mesh.py
@@ -17,6 +17,5 @@ def soup2mesh(P,T):
         the unique points, the new topology tensor, the indices of the unique points and the mapping of the old points
     """
 
-    P,I,J = uniquetol(P,tol=0.0001,ByRows=True)
-    T     = J[T]
-    return P,T,I,J
+    I,J = uniquetol(P,tol=0.0001,ByRows=True)[1:]
+    return P[I],J[T],I,J
