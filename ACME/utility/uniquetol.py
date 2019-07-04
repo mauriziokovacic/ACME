d5b37401a28ef28bde00f336d530a9404c7442d6
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-07-04 11:52:47 +0200

    Forced PEP

--- ACME/utility/uniquetol.py
+++ ACME/utility/uniquetol.py
@@ -4,7 +4,8 @@ from .col         import *
 from .row         import *
 from .numpy2torch import *
 
-def uniquetol(A,tol=10e-4,ByRows=False):
+
+def uniquetol(A, tol=10e-4, ByRows=False):
     """
     Returns the unique elements in A using tolerance tol.
 
@@ -27,29 +28,30 @@ def uniquetol(A,tol=10e-4,ByRows=False):
         returns the tensor of unique values/rows, their indices within the input tensor and the input tensor indices within the unique
     """
 
-    C  = A.cpu().numpy()
+    C = A.cpu().numpy()
     if ByRows:
         c  = []
-        for i in range(col(A)-1,-1,-1):
-            c = c+[C[:,i]]
-        i  = numpy.lexsort(c,axis=0)
-        d  = numpy.reshape(numpy.append(numpy.ones((1,col(C)))+tol,numpy.abs(numpy.diff(C[i],axis=0))),(-1,col(A)))
-        tf = numpy.sum(d>tol,1)>0
+        for i in range(col(A) - 1, -1, -1):
+            c = c + [C[:, i]]
+        i  = numpy.lexsort(c, axis=0)
+        d  = numpy.reshape(numpy.append(numpy.ones((1, col(C))) + tol, numpy.abs(numpy.diff(C[i], axis=0))),
+                           (-1, col(A)))
+        tf = numpy.sum(d > tol, 1) > 0
         n  = row(A)
     else:
         C  = numpy.ravel(C)
         i  = numpy.argsort(C)
-        d  = numpy.append(1+tol,numpy.diff(C[i]))
-        tf = d>(tol*numpy.max(numpy.abs(C)))
+        d  = numpy.append(1 + tol, numpy.diff(C[i]))
+        tf = d > (tol * numpy.max(numpy.abs(C)))
         n  = numpy.size(A)
     ia = i[tf]
     C  = C[ia]
-    ic = numpy.zeros(n,dtype=int)
-    j = -1
-    for x in range(0,n):
+    ic = numpy.zeros(n, dtype=int)
+    j  = -1
+    for x in range(0, n):
         if tf[x]:
             j += 1
         ic[i[x]] = j
-    return numpy2torch(C, dtype=A.dtype,   device=A.device),\
-           numpy2torch(ia,dtype=torch.long,device=A.device),\
-           numpy2torch(ic,dtype=torch.long,device=A.device)
+    return numpy2torch(C,  dtype=A.dtype,    device=A.device), \
+           numpy2torch(ia, dtype=torch.long, device=A.device), \
+           numpy2torch(ic, dtype=torch.long, device=A.device)
