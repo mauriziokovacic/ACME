cc8b7b23175f7a24f718d0c345ff5989bcbbdf4f
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-07-05 09:12:23 +0200

    Improved code

--- ACME/geometry/vertex_normal.py
+++ ACME/geometry/vertex_normal.py
@@ -5,7 +5,8 @@ from ..math.cross         import *
 from ..math.normvec       import *
 from .triangle_angle      import *
 
-def vertex_normal(P,T,type=None):
+
+def vertex_normal(P, T, type=None):
     """
     Returns the normal of the vertices in the triangulation, averaged with the
     specified method.
@@ -25,24 +26,24 @@ def vertex_normal(P,T,type=None):
         the vertex normals of the input mesh
     """
 
-    Pi,Pj,Pk = P[T]
-    Ni = cross(Pj-Pi,Pk-Pi)
-    Nj = cross(Pk-Pj,Pi-Pj)
-    Nk = cross(Pi-Pk,Pj-Pk)
+    Pi, Pj, Pk = P[T]
+    Ni = cross(Pj - Pi, Pk - Pi)
+    Nj = cross(Pk - Pj, Pi - Pj)
+    Nk = cross(Pi - Pk, Pj - Pk)
     if type is not None:
-        if strcmpi(type,'area'):
-            Ai = torch.mul(norm(Ni),0.5)
-            Aj = torch.mul(norm(Nj),0.5)
-            Ak = torch.mul(norm(Nk),0.5)
-            Ni = normr(Ni)*Ai
-            Nj = normr(Nj)*Aj
-            Nk = normr(Nk)*Ak
-        if strcmpi(type,'angle'):
-            Ai,Aj,Ak = triangle_angle(P,T)
-            Ni = normr(Ni)*Ai
-            Nj = normr(Nj)*Aj
-            Nk = normr(Nk)*Ak
+        if strcmpi(type, 'area'):
+            Ai = torch.mul(norm(Ni), 0.5)
+            Aj = torch.mul(norm(Nj), 0.5)
+            Ak = torch.mul(norm(Nk), 0.5)
+            Ni = normr(Ni) * Ai
+            Nj = normr(Nj) * Aj
+            Nk = normr(Nk) * Ak
+        if strcmpi(type, 'angle'):
+            Ai, Aj, Ak = triangle_angle(P, T)
+            Ni = normr(Ni) * Ai
+            Nj = normr(Nj) * Aj
+            Nk = normr(Nk) * Ak
     I = torch.cat(tuple(T)).squeeze()
-    V = torch.cat((Ni,Nj,Nk),dim=0)
-    N = accumarray(I,V,size=row(P))
+    V = torch.cat((Ni, Nj, Nk), dim=0)
+    N = accumarray(I, V, size=P.size())
     return normr(N)
