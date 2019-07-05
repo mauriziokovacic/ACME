c1485caddbc0ef8c5ccac9926414b3975e75867b
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-07-05 09:08:58 +0200

    Improved readability

--- ACME/utility/row.py
+++ ACME/utility/row.py
@@ -1,6 +1,7 @@
 from .size import *
 from .ndim import *
 
+
 def row(A):
     """
     Returns the number of rows in the given Tensor
@@ -16,6 +17,6 @@ def row(A):
         the number of rows
     """
 
-    if ndim(A)>=1:
+    if ndim(A) >= 1:
         return size(A)[0]
     return 0
