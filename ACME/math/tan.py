f4ce85357747bfc8e392ef19b93eace33a37083e
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-07-12 16:20:24 +0200

    Improved generality of the tan function

--- ACME/math/tan.py
+++ ACME/math/tan.py
@@ -1,24 +1,37 @@
-from .norm  import *
-from .dot   import *
-from .cross import *
+import numpy
+import torch
+from ..utility.isnumpy  import *
+from ..utility.istorch  import *
+from ..utility.isscalar import *
+from .norm              import *
+from .dot               import *
+from .cross             import *
 
 
-def tan(A, B, dim=1):
+def tan(A, B=None, dim=1):
     """
     Computes the tangent value of the angle between the inputs along the specified dimension
 
     Parameters
     ----------
-    A : Tensor
+    A : Tensor or scalar
         the first input tensor
-    B : Tensor
-        the second input tensor
+    B : Tensor (optional)
+        the second input tensor (default is None)
     dim : int (optional)
-        dimension along the tangent is computed
+        dimension along the tangent is computed, if B is a Tensor (default is 1)
 
     Returns
     -------
-    Tensor
-        the tensor containing the tangent values
+    Tensor or scalar
+        the tensor (scalar) containing the tangent values
     """
+
+    if B is None:
+        if isnumpy(A):
+            return numpy.tan(A)
+        if istorch(A):
+            return torch.tan(A)
+        if isscalar(A):
+            return math.tan(A)
     return norm(cross(A, B, dim=dim))/dot(A, B, dim=dim)
