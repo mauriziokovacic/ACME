fa5f2174bedf48502a0d51313f41b058536d9a9b
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-09-23 19:51:28 +0200

    Added edge utilities

--- topology/poly2edge.py
+++ topology/poly2edge.py
@@ -4,7 +4,7 @@ from ..utility.col       import *
 from ..utility.repmat    import *
 from ..utility.circshift import *
 from ..utility.indices   import *
-from .ind2poly           import *
+from .poly2unique        import *
 
 
 def poly2edge(T):
@@ -28,8 +28,8 @@ def poly2edge(T):
     """
 
     assert row(T)>1, "Topology matrix should be at least of size 2xt"
-    E = torch.cat((torch.cat(tuple(T),                   ).unsqueeze(0),
-                   torch.cat(tuple(circshift(T,-1,dim=0))).unsqueeze(0)), dim=0)
+    E = torch.cat((torch.cat(tuple(T),                     ).unsqueeze(0),
+                   torch.cat(tuple(circshift(T, -1, dim=0))).unsqueeze(0)), dim=0)
     F = repmat(indices(0, col(T)-1, device=T.device), row(T), 1)
     return E, F
 
@@ -67,3 +67,11 @@ def quad2edge(T):
         the edge tensor and the respective quad indices
     """
     return poly2edge(T)
+
+
+def edge2undirect(E):
+    return poly2edge(poly2unique(E))[0]
+
+
+def poly2undirect(T):
+    return edge2undirect(poly2edge(T)[0])
