16e7810dc33affb22ef574ca638f769d0aba7b99
root <pizza_lae@here>
2019-09-24 10:30:51 +0200

    Added normal metric

--- loss/__init__.py
+++ loss/__init__.py
@@ -2,3 +2,4 @@ from .Loss             import *
 from .LossList         import *
 from .GlobalLoss       import *
 from .hausdorff_metric import *
+from .normal_metric    import *
---
+++ loss/normal_metric.py
@@ -0,0 +1,31 @@
+import torch
+from ..math.dot           import *
+from ..math.knn           import *
+from ..math.unrooted_norm import *
+
+
+def normal_metric(Pi, Ni, Pj, Nj):
+    """
+    Returns the normal metric for two given oriented point sets
+
+    Parameters
+    ----------
+    Pi : Tensor
+        a (M,3,) points set tensor
+    Ni : Tensor
+        a (M,3,) normal tensor
+    Pj : Tesnor
+        a (N,3,) points set tensor
+    Nj : Tensor
+        a (M,3,) normal tensor
+
+    Returns
+    -------
+    Tensor
+        a (1,) metric tensor
+    """
+
+    i = knn(Pi, Pj, 1, distFcn=sqdistance)[0].squeeze()
+    j = knn(Pj, Pi, 1, distFcn=sqdistance)[0].squeeze()
+    return torch.sum(sqdistance(Ni, Nj[i,:], dim=1)) + \
+           torch.sum(sqdistance(Nj, Ni[j,:], dim=1))
