Fixed minor bugs and attempted Jacobian

geometry/metric/quadrilateral/quadrilateral_distortion_metric.py
@@ -14,6 +14,4 @@ class QuadrilateralDistortionMetric(Metric):
         )
 
     def eval(self, P, T):
-        A = QuadrilateralAreaMetric().eval(P, T)
-
-        return (4 * torch.det(J)) / A
+        raise NotImplementedError

geometry/metric/quadrilateral/quadrilateral_radius_ratio_metric.py
@@ -18,4 +18,4 @@ class QuadrilateralRadiusRatioMetric(QuadrilateralMetric):
         hmax = torch.max(torch.max(L, dim=1, keepdim=True)[0], self.max_diagonal_length(P, T), keepdim=True)
         L2   = torch.sum(torch.pow(L, 2), dim=1, keepdim=True)
         A    = torch.abs(a / 2)
-        return (L2*hmax)/torch.min(A, dim=1, keepdim=True)[0]
+        return (L2*hmax) / torch.min(A, dim=1, keepdim=True)[0]

geometry/metric/quadrilateral/quadrilateral_warpage_metric.py
@@ -13,7 +13,7 @@ class QuadrilateralWarpageMetric(QuadrilateralMetric):
         )
 
     def eval(self, P, T):
-        n0, n1, n2, n3 = self.corner_normalized_normals(P, T)
+        n0, n1, n2, n3 = self.normalized_corner_normals(P, T)
         return 1 - torch.min(torch.pow(dot(n0, n2, dim=1), 3),
                              torch.pow(dot(n1, n3, dim=1), 3), keepdim=True)
 

geometry/metric/triangle/triangle_distortion_metric.py
@@ -13,4 +13,11 @@ class TriangleDistortionMetric(TriangleMetric):
         )
 
     def eval(self, P, T):
-        raise NotImplementedError
+        L0, L1, L2 = self.edges(P, T)
+        A          = self.area(P, T)
+        ######## How the fudge should I do this?
+        J = torch.cat((L2.unsqueeze(2), -L1.unsqueeze(2)), dim=2)
+        J = torch.matmul(J.permute(0, 2, 1), J)
+        J = torch.det(J).unsqueeze(1)
+        ########
+        return (J * SQRT3) / A

geometry/metric/triangle/triangle_scaled_jacobian_metric.py
@@ -19,7 +19,11 @@ class TriangleScaledJacobianMetric(TriangleMetric):
         Lmax = torch.max(Lmax,
                          norm(L1, dim=1) * norm(L2, dim=1), keepdim=True)[0]
         n = self.normal(P, T)
-        J = torch.det()
+        ######## How the fudge should I do this?
+        J = torch.cat((L2.unsqueeze(2), -L1.unsqueeze(2)), dim=2)
+        J = torch.matmul(J.permute(0, 2, 1), J)
+        J = torch.det(J).unsqueeze(1)
+        ########
         if dot(n, cross(L2, L1, dim=1), dim=1):
             J = -J
         return (2*SQRT3)/3 * (J / Lmax)