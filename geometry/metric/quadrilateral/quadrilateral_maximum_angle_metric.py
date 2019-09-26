7f9921d3b0f5eb8b6dc42200bc84daa52bcd3351
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-09-26 15:36:24 +0200

    Fixed ranges

--- geometry/metric/quadrilateral/quadrilateral_maximum_angle_metric.py
+++ geometry/metric/quadrilateral/quadrilateral_maximum_angle_metric.py
@@ -5,10 +5,10 @@ class QuadrilateralMaximumAngleMetric(QuadrilateralMetric):
     def __init__(self):
         super(QuadrilateralMaximumAngleMetric, self).__init__(
             name='Quadrilateral Maximum Angle',
-            dimension='1',
-            acceptable_range=Range(min=1, max=1.3),
-            normal_range=Range(min=1, max=Inf),
-            full_range=Range(min=1, max=Inf),
-            q_for_unit=1,
+            dimension='A^1',
+            acceptable_range=Range(min=90.0, max=135.0),
+            normal_range=Range(min=90.0, max=360.0),
+            full_range=Range(min=0.0, max=360.0),
+            q_for_unit=90.0,
         )
 
--- geometry/metric/quadrilateral/quadrilateral_minimum_angle_metric.py
+++ geometry/metric/quadrilateral/quadrilateral_minimum_angle_metric.py
@@ -3,7 +3,11 @@ from .quadrilateral_metric import *
 
 class QuadrilateralMinimumAngleMetric(QuadrilateralMetric):
     def __init__(self):
-        pass
-
-    def eval(self, P, T):
-        pass
+        super(QuadrilateralMinimumAngleMetric, self).__init__(
+            name='Quadrilateral Minimum Angle',
+            dimension='A^1',
+            acceptable_range=Range(min=45.0, max=90.0),
+            normal_range=Range(min=0.0, max=90.0),
+            full_range=Range(min=0.0, max=360.0),
+            q_for_unit=90.0,
+        )
