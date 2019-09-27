8c5f39f98891fa38767736176f82ac62c0c6a4c5
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-09-27 12:43:18 +0200

    Imrpoved code

--- utility/clamp.py
+++ utility/clamp.py
@@ -1,8 +1,9 @@
 import math
 import torch
-from .istorch import *
-from .size import *
-from .ndim import *
+from .istorch        import *
+from .size           import *
+from .ndim           import *
+from .ConstantTensor import *
 
 
 def clamp(a, inf=0, sup=1):
@@ -13,9 +14,9 @@ def clamp(a, inf=0, sup=1):
     ----------
     a : int,float or Tensor
         input value
-    inf : int or float (optional)
+    inf : int or float or Tensor (optional)
         the inferior bound (default is 0)
-    sup : int or float (optional)
+    sup : int or float or Tensor (optional)
         the superior bound (default is 1)
 
     Returns
@@ -25,7 +26,13 @@ def clamp(a, inf=0, sup=1):
     """
 
     if istorch(a):
-        return torch.clamp(a, inf, sup)
+        if isscalar(inf) and isscalar(sup):
+            return torch.clamp(a, inf, sup)
+        if not istorch(inf):
+            inf = ConstantTensor(inf, 1, dtype=a.dtype, device=a.device)
+        if not istorch(sup):
+            sup = ConstantTensor(sup, 1, dtype=a.dtype, device=a.device)
+        return torch.min(torch.max(a, inf.expand(*a.shape[:-1], -1)), sup.expand(*a.shape[:-1], -1))
     return min(max(inf, a), sup)
 
 
@@ -67,4 +74,3 @@ def clamp_min(a, inf=0):
     """
 
     return clamp(a, inf=inf, sup=math.inf)
-
