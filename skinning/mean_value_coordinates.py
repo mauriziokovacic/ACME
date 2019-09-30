108067d30a618b85a51a889f92f7307ef3ec9985
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-09-30 15:16:53 +0200

    Added mean values coordinates deformation

---
+++ skinning/mean_value_coordinates.py
@@ -0,0 +1,21 @@
+import torch
+
+
+def cage_deformation(C, W):
+    """
+    Returns the points set defined by the cage points
+
+    Parameters
+    ----------
+    C : Tensor
+        the (M,3,) cage points tensor
+    W : Tensor or SparseTensor
+        the (N,M,) weights tensor
+
+    Returns
+    -------
+    Tensor
+        a (N,3,) points set tensor
+    """
+
+    return torch.matmul(W, C)
