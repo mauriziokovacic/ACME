dd8e4b0cd13dbc08938907461d4bc1177ac948ff
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-01 12:07:06 +0200

    Refactored interface

--- model/model.py
+++ model/model.py
@@ -1,6 +1,6 @@
-import os
 import warnings
 import torch
+from ..fileio.fileparts import *
 
 
 class Model(torch.nn.Module):
@@ -11,6 +11,8 @@ class Model(torch.nn.Module):
     ----------
     name : str
         the name of the model
+    device : str or torch.device
+        the device to store the tensors to
 
     Methods
     -------
@@ -21,19 +23,26 @@ class Model(torch.nn.Module):
     save_model(path)
         stores the model state in the given path
     load_model(path)
-        loads a model form the given path
+        loads a model from the given path
+    save_checkpoint(path)
+        stores the model checkpoint in the given path
+    load_checkpoint(path)
+        loads the model checkpoint from the given path
     """
 
-    def __init__(self, name='Model'):
+    def __init__(self, name='Model', device='cuda:0'):
         """
         Parameters
         ----------
         name : str (optional)
             the name of the model (default is 'Model')
+        device : str or torch.device (optional)
+            the device to store the tensors to (default is 'cuda:0')
         """
 
         super(Model, self).__init__()
-        self.name = name
+        self.name   = name
+        self.device = device
         
     def freeze(self):
         """
@@ -63,36 +72,104 @@ class Model(torch.nn.Module):
             param.requires_grad = True
         return self
 
-    def save_model(self, path=None):
+    def save_model(self, filename):
         """
-        Stores the model state in the given path
+        Stores the model state in the given filename.
+
+        The file extension will be forced to '.pth'
 
         Parameters
         ----------
-        path : str (optional)
-            the path to store the model to. If None it will be set to the current working directory (default is None)
+        filename : str
+            the filename to store the model to
+
+        Returns
+        -------
+        Model
+            the model itself
         """
 
-        if path is None:
-            path = os.getcwd() + '/' + self.name + '.pth'
-        torch.save(self, path)
+        path, file = fileparts(filename)[:2]
+        torch.save(self, path + file + '.pth')
         return self
 
-    def load_model(self, path=None, device='cuda:0'):
+    def load_model(self, filename, strict=True):
         """
-        Loads a model from the given path
+        Loads a model from the given filename
+
+        The file extension will be forced to '.pth'
 
         Parameters
         ----------
-        path : str (optional)
-            the path to load the model from. If None it will be set to the current working directory (default is None)
+        filename : str
+            the filename to load the model from
+        strict : bool (optional)
+            if True loaded model must completely match with this. If False only compatible parameters will be loaded
+
+        Returns
+        -------
+        Model
+            the model itself
         """
 
-        if path is None:
-            path = os.getcwd() + '/' + self.name + '.pth'
-        if not os.path.isfile(path):
-            warnings.warn('File ' + path + ' does not exists.')
-            return
-        self = torch.load(path, map_location=device)
+        path, file = fileparts(filename)[:2]
+        other = torch.load(path + file + '.pth', map_location=self.device)
+        if strict and (not isinstance(other, self.__class__)):
+            warnings.warn('Class type mismatch. Loading model failed', category=RuntimeWarning)
+            return self
+        self.load_state_dict(other.state_dict(), map_location=self.device, strict=strict)
         self.eval()
         return self
+
+    def save_checkpoint(self, filename):
+        """
+        Stores the model checkpoint in the given filename
+
+        The file extension will be forced to '.tar'
+
+        Parameters
+        ----------
+        filename : str
+            the filename to store the model checkpoint to
+
+        Returns
+        -------
+        Model
+            the model itself
+        """
+
+        path, file = fileparts(filename)[:2]
+        torch.save({'model_state_dict': self.state_dict()}, path + file + '.tar')
+        return self
+
+    def load_checkpoint(self, filename, strict=True):
+        """
+        Loads the model checkpoint from the given filename
+
+        The file extension will be forced to '.tar'
+
+        Parameters
+        ----------
+        filename : str
+            the filename to load the model checkpoint from
+        strict : bool (optional)
+            if True loaded checkpoint must completely match with the model parameters.
+            If False only compatible parameters will be loaded
+
+        Returns
+        -------
+        Model
+            the model itself
+        """
+
+        path, file = fileparts(filename)[:2]
+        checkpoint = torch.load(path + file + '.tar')
+        self.load_state_dict(checkpoint['model_state_dict'], map_location=self.device, strict=strict)
+        self.to(self.device)
+        self.train()
+        return self
+
+    def __setattr__(self, key, value):
+        if key == 'device':
+            self.to(device=value)
+        self.__dict__[key] = value
