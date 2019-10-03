40033aa8adb711b591c16db60a0276b8a463549f
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-03 12:32:27 +0200

    Ammortized loading and saving functionality

--- color/palette.py
+++ color/palette.py
@@ -1,9 +1,11 @@
 import os
 import os.path
-from ..fileio.fileparts import *
-from ..fileio.PNGfile import *
+from ..fileio.fileparts    import *
+from ..fileio.PNGfile      import *
+from ..utility.static_vars import *
 
 
+@static_vars(cdata=dict())
 def palette(name, device='cuda:0'):
     """
     Returns the color palette with the specified name
@@ -28,9 +30,40 @@ def palette(name, device='cuda:0'):
         if the name is unknown
     """
 
+    if name not in palette.cdata:
+        path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'asset')
+        filename = [fileparts(f)[1] for f in os.listdir(path)
+                    if os.path.isfile(os.path.join(path, f)) and f.endswith('.png')]
+        if name not in filename:
+            raise RuntimeError('Palette \'{}\' not present. Choose one from the following:\n{}'.format(name, '\n'.join(filename)))
+        palette.cdata[name] = import_PNG(os.path.join(path, name + '.png'), device=device).squeeze()
+    return palette.cdata[name]
+
+
+def save_palette(name, cdata):
+    """
+    Saves the given palette to the asset folder
+
+    Parameters
+    ----------
+    name : str
+        name of the palette
+    cdata : Tensor
+        the (N,3,) color tensor
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    RuntimeError
+        if name is already in use
+    """
+
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'asset')
     filename = [fileparts(f)[1] for f in os.listdir(path)
                 if os.path.isfile(os.path.join(path, f)) and f.endswith('.png')]
-    if name not in filename:
-        raise RuntimeError('Palette \'{}\' not present. Choose one from the following:\n{}'.format(name, '\n'.join(filename)))
-    return import_PNG(os.path.join(path, name + '.png'), device=device).squeeze()
+    if name in filename:
+        raise RuntimeError('Palette \'{}\' already present. Choose another name')
+    export_PNG(os.path.join(path, name + '.png'), cdata.unsqueeze(0))
