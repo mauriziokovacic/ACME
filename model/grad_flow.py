6744c8023497c6a3b7ab6adce2f7d0765e9a9464
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-16 15:14:34 +0200

    Improved interface

--- model/grad_flow.py
+++ model/grad_flow.py
@@ -1,11 +1,11 @@
-def grad_flow(named_parameters):
+def grad_flow(model):
     """
     Returns a dictionary containing the [min, mean, max] of each layer gradient
 
     Parameters
     ----------
-    named_parameters : generator
-        the generator returned by the named_parameters function of a model
+    model : torch.nn.Module
+        the module to extract the gradient flow from
 
     Returns
     -------
@@ -14,7 +14,7 @@ def grad_flow(named_parameters):
     """
 
     d = {}
-    for n, p in named_parameters:
+    for n, p in model.named_parameters():
         if p.requires_grad and ("bias" not in n):
             g = p.grad.abs()
             d[n] = [g.min().item(), g.mean().item(), g.max().item()]
