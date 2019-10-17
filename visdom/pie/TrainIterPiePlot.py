54c84d50992cf3c69fc97ae532b775f8fe8a4a75
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-17 11:09:03 +0200

    Added time visualization

--- visdom/pie/TrainIterPiePlot.py
+++ visdom/pie/TrainIterPiePlot.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from .PiePlot import *
 
 
@@ -5,7 +6,7 @@ class TrainIterPiePlot(PiePlot):
     def __init__(self, session, win='TrainIterPiePlot'):
         super(TrainIterPiePlot, self).__init__(session, win=win, title='Training Process')
 
-    def __update__(self, epoch, iter, **kwargs):
+    def __update__(self, epoch, iter, time=None, **kwargs):
         d = epoch[0] / epoch[1]
         e = 1 / epoch[1]
         t = 1 - (e+d)
@@ -14,11 +15,14 @@ class TrainIterPiePlot(PiePlot):
             i = 0
             d = d + e
         e = 1 - (t+d+i)
+        if time is not None:
+            g = (epoch[0] * iter[1] + iter[0], epoch[1] * iter[1])
+            self.__fig__.update_layout(title={'text': 'ETA: {}'.format(timedelta(seconds=time * (g[1]-g[0])))})
         super(TrainIterPiePlot, self).__update__(
             labels=['Done', 'Processing', 'Epoch', 'Missing'],
             values=[d, i, e, t],
             marker_colors=['rgb(34, 177, 76)',   # green
-                           'rgb(255, 163, 23)',   # orange
+                           'rgb(255, 163, 23)',  # orange
                            'rgb(237, 28, 36)',   # red
                            'rgb(0, 102, 232)'],  # blue
             **kwargs
