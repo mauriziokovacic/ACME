8fe5a4affd412d1264b0369db8c6f5957a393274
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-18 17:34:49 +0200

    Changed naming

--- layer/FixedGraphConv.py
+++ layer/FixedGraphConv.py
@@ -2,9 +2,9 @@ import torch
 from .init import *
 
 
-class CachedGraphConv(torch.nn.Module):
+class FixedGraphConv(torch.nn.Module):
     def __init__(self, in_channels, out_channels, matrix, bias=False):
-        super(CachedGraphConv, self).__init__()
+        super(FixedGraphConv, self).__init__()
         self.weight = torch.nn.Parameter(torch.Tensor(1, in_channels, out_channels))
         if bias:
             self.bias = torch.nn.Parameter(torch.Tensor(1, out_channels))
@@ -18,11 +18,8 @@ class CachedGraphConv(torch.nn.Module):
         init(self.bias, 'zero')
 
     def forward(self, x):
-        y = x
-        if y.ndimension() == 2:
-            y = y.unsqueeze(0)
         b = 0 if self.bias is None else self.bias
-        return (torch.matmul(self.matrix, torch.matmul(y, self.weight)) + b).squeeze()
+        return (torch.matmul(self.matrix, torch.matmul(x, self.weight)) + b).squeeze()
 
     def extra_repr(self):
         return '{}, {}, bias={}'.format(self.weight.size(-2), self.weight.size(-1), self.bias is not None)
