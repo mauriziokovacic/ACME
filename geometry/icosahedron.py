8d7f345ffeb5d1c7f5b27f76aa7b61a5a380ef72
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-23 12:14:30 +0200

    Improved PEP

--- geometry/icosahedron.py
+++ geometry/icosahedron.py
@@ -20,24 +20,24 @@ def Icosahedron(device='cuda:0'):
         the point set tensor, the topology tensor, the vertex normals
     """
 
-    P = FloatTensor([[ 0       ,-0.525731, 0.850651],
-                     [ 0.850651, 0       , 0.525731],
-                     [ 0.850651, 0       ,-0.525731],
-                     [-0.850651, 0       ,-0.525731],
-                     [-0.850651, 0       , 0.525731],
-                     [-0.525731, 0.850651, 0],
-                     [ 0.525731, 0.850651, 0],
-                     [ 0.525731,-0.850651, 0],
-                     [-0.525731,-0.850651, 0],
-                     [ 0       ,-0.525731,-0.850651],
-                     [ 0       , 0.525731,-0.850651],
-                     [ 0       , 0.525731, 0.850651]],device=device)
+    P = FloatTensor([[ 0       , -0.525731,  0.850651],
+                     [ 0.850651,  0       ,  0.525731],
+                     [ 0.850651,  0       , -0.525731],
+                     [-0.850651,  0       , -0.525731],
+                     [-0.850651,  0       ,  0.525731],
+                     [-0.525731,  0.850651,  0],
+                     [ 0.525731,  0.850651,  0],
+                     [ 0.525731, -0.850651,  0],
+                     [-0.525731, -0.850651,  0],
+                     [ 0       , -0.525731, -0.850651],
+                     [ 0       ,  0.525731, -0.850651],
+                     [ 0       ,  0.525731,  0.850651]], device=device)
     T = torch.add(torch.t(LongTensor([
-        [ 2, 3, 7],[ 2, 8, 3],[ 4, 5, 6],[ 5, 4, 9],
-        [ 7, 6,12],[ 6, 7,11],[10,11, 3],[11,10, 4],
-        [ 8, 9,10],[ 9, 8, 1],[12, 1, 2],[ 1,12, 5],
-        [ 7, 3,11],[ 2, 7,12],[ 4, 6,11],[ 6, 5,12],
-        [ 3, 8,10],[ 8, 2, 1],[ 4,10, 9],[ 5, 9, 1]], device=device)), -1)
+        [2, 3,  7], [2, 8,  3], [ 4,  5, 6], [ 5,  4,  9],
+        [7, 6, 12], [6, 7, 11], [10, 11, 3], [11, 10,  4],
+        [8, 9, 10], [9, 8,  1], [12,  1, 2], [ 1, 12,  5],
+        [7, 3, 11], [2, 7, 12], [ 4,  6,11], [ 6,  5, 12],
+        [3, 8, 10], [8, 2,  1], [ 4, 10, 9], [ 5,  9,  1]], device=device)), -1)
     N = normr(P.clone())
     return P, T, N
 
@@ -57,8 +57,8 @@ def Icosahedron_2(device='cuda:0'):
         the point set tensor, the topology tensor, the vertex normals
     """
 
-    P, T = Icosahedron(device=device)[0:2]
-    P, T = subdivide(P, T, 1)[0:2]
+    P, T = Icosahedron(device=device)[:2]
+    P, T = subdivide(P, T, 1)[:2]
     P    = normr(P)
     N    = P.clone()
     return P, T, N
@@ -79,8 +79,8 @@ def Icosahedron_3(device='cuda:0'):
         the point set tensor, the topology tensor, the vertex normals
     """
 
-    P, T = Icosahedron(device=device)[0:2]
-    P, T = subdivide(P,T,2)[0:2]
+    P, T = Icosahedron(device=device)[:2]
+    P, T = subdivide(P, T, 2)[:2]
     P    = normr(P)
     N    = P.clone()
     return P, T, N
