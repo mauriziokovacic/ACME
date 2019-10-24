b8f38fca73a223766be1c6c3f43a7c5b2c6af816
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-10-24 16:54:23 +0200

    Fixed useless slicing

--- transform/Normalize.py
+++ transform/Normalize.py
@@ -9,7 +9,7 @@ class Normalize(Transform):
     def __eval__(self, x, *args, **kwargs):
         min = torch.min(x.pos, dim=0)[0]
         max = torch.max(x.pos, dim=0)[0]
-        d = torch.max(max - min)[0]
+        d = torch.max(max - min)
         c = 0.5 * (max + min)
         x.pos -= c
         x.pos *= 2/d
