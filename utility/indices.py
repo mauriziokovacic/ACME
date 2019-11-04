f96aae18006d0324e51c21be6160f0fb96770928
mauriziokovacic <maurizio.kovacic@gmail.com>
2019-11-04 15:53:09 +0100

    Tested performances. Palm tree wins

--- utility/indices.py
+++ utility/indices.py
@@ -26,5 +26,4 @@ def indices(min, max, dtype=torch.long, device='cuda:0'):
     """
     min = round(min)
     max = round(max)
-    s   = (-1)**(min > max)
-    return torch.arange(start=min, end=max+s, step=s, dtype=dtype, device=device).unsqueeze(1)
+    return linspace(min=min, max=max, n=abs(max-min)+1, dtype=dtype, device=device)
