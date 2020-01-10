5d64d6ce3360ed6b5dc615c014605c467d6e5e76
mauriziokovacic <maurizio.kovacic@gmail.com>
2020-01-10 15:22:26 +0100

    Improved init and fixed useless imports

--- ACME/color/color2gray.py
+++ ACME/color/color2gray.py
@@ -1,10 +1,9 @@
 from .color2float import *
-from ..utility.strcmpi import *
 
 
 def color2gray(C, weight=(1/3, 1/3, 1/3), dim=-1):
     """
-    Converts the given color in grayscale using the specified method
+    Converts the given color in grayscale using the specified weights
 
     Parameters
     ----------
@@ -28,12 +27,60 @@ def color2gray(C, weight=(1/3, 1/3, 1/3), dim=-1):
 
 
 def color2gray_pal(C, dim=-1):
+    """
+    Converts the given color in grayscale with PAL standard
+
+    Parameters
+    ----------
+    C : Tensor
+        the (3,W,H,) or (N,3,) RGB color tensor
+    dim : int (optional)
+        the dimension along the operation is performed (default is -1)
+
+    Returns
+    -------
+    Tensor
+        the (W,H,) or (N,) grayscale color tensor
+    """
+
     return color2gray(C, weight=[0.299, 0.587, 0.114], dim=dim)
 
 
 def color2gray_hdtv(C, dim=-1):
+    """
+    Converts the given color in grayscale with HDTV standard
+
+    Parameters
+    ----------
+    C : Tensor
+        the (3,W,H,) or (N,3,) RGB color tensor
+    dim : int (optional)
+        the dimension along the operation is performed (default is -1)
+
+    Returns
+    -------
+    Tensor
+        the (W,H,) or (N,) grayscale color tensor
+    """
+
     return color2gray(C, weight=[0.2126, 0.7152, 0.0722], dim=-1)
 
 
 def color2gray_hdr(C, dim=-1):
+    """
+    Converts the given color in grayscale with HDR standard
+
+    Parameters
+    ----------
+    C : Tensor
+        the (3,W,H,) or (N,3,) RGB color tensor
+    dim : int (optional)
+        the dimension along the operation is performed (default is -1)
+
+    Returns
+    -------
+    Tensor
+        the (W,H,) or (N,) grayscale color tensor
+    """
+
     return color2gray(C, weight=[0.2627, 0.6780, 0.0593], dim=dim)
--- ACME/color/colors.py
+++ ACME/color/colors.py
@@ -3,56 +3,182 @@ from .color2float          import *
 
 
 def black():
+    """
+    Returns the black RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) black tensor
+    """
+
     return torch.zeros(1, 3)
 
 
 def white():
+    """
+    Returns the white RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) white tensor
+    """
+
     return torch.ones(1, 3)
 
 
 def red():
+    """
+    Returns the red RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) red tensor
+    """
+
     return color2float(Uint8Tensor([237, 28, 36]))
 
 
 def green():
+    """
+    Returns the green RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) green tensor
+    """
+
     return color2float(Uint8Tensor([34, 177, 76]))
 
 
 def blue():
+    """
+    Returns the blue RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) blue tensor
+    """
+
     return color2float(Uint8Tensor([0, 162, 232]))
 
 
 def cyan():
+    """
+    Returns the cyan RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) cyan tensor
+    """
+
     return torch.add(torch.neg(red()), 1)
 
 
 def magenta():
+    """
+    Returns the magenta RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) magenta tensor
+    """
+
     return torch.add(torch.neg(green()), 1)
 
 
 def yellow():
-    return color2float(Uint8Tensor([255, 242, 0]))
+    """
+    Returns the yellow RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) yellow tensor
+    """
+
+    return color2float(Uint8Tensor([[255, 242, 0]]))
 
 
 def brown():
-    return color2float(Uint8Tensor([149, 116, 83]))
+    """
+    Returns the brown RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) brown tensor
+    """
+
+    return color2float(Uint8Tensor([[149, 116, 83]]))
 
 
 def dark_teal():
-    return color2float(Uint8Tensor([98, 140, 178]))
+    """
+    Returns the dark teal RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) dark teal tensor
+    """
+
+    return color2float(Uint8Tensor([[98, 140, 178]]))
 
 
 def gray():
+    """
+    Returns the gray RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) gray tensor
+    """
+
     return torch.mul(torch.ones(1, 3), 0.5)
 
 
 def orange():
-    return color2float(Uint8Tensor([253, 135, 86]))
+    """
+    Returns the orange RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) orange tensor
+    """
+
+    return color2float(Uint8Tensor([[253, 135, 86]]))
 
 
 def pink():
-    return color2float(Uint8Tensor([254, 194, 194]))
+    """
+    Returns the pink RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) pink tensor
+    """
+
+    return color2float(Uint8Tensor([[254, 194, 194]]))
 
 
 def teal():
-    return color2float(Uint8Tensor([144, 216, 196]))
+    """
+    Returns the teal RGB tensor
+
+    Returns
+    -------
+    Tensor
+        the (1,3,) teal tensor
+    """
+
+    return color2float(Uint8Tensor([[144, 216, 196]]))
