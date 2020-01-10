0a3ef3f552003235db85751e9871eb1afa2e746c
mauriziokovacic <maurizio.kovacic@gmail.com>
2020-01-10 15:22:44 +0100

    Improved init

--- ACME/encoding/__init__.py
+++ ACME/encoding/__init__.py
@@ -1,4 +1,3 @@
-from .sph2color      import *
 from .color2normal   import *
 from .color2position import *
 from .color2sph      import *
@@ -8,3 +7,17 @@ from .label2index    import *
 from .normal2color   import *
 from .onehot2index   import *
 from .position2color import *
+from .sph2color      import *
+
+__all__ = [
+    'color2normal',
+    'color2position',
+    'color2sph',
+    'index2label',
+    'index2onehot',
+    'label2index',
+    'normal2color',
+    'onehot2index',
+    'position2color',
+    'sph2color',
+]
